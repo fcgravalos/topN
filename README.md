d55d1ce24bac680dff12062c6ef8c8386fe48583
Fernando Crespo Grávalos <fcgravalos@email.com>
2016-11-28 00:16:03 +0100

    README and simple improvements to file_generator

---
+++ README.md
@@ -0,0 +1,7 @@
+### topN
+
+Usage:
+  - Create a numbers file: `ruby file_generator.rb numbers.txt n_lines
+    max`
+  - From that file: `ruby topN.rb numbers.txt N`
+
--- file_generator.rb
+++ file_generator.rb
@@ -1,7 +1,9 @@
 filename = File.expand_path(ARGV[0])
+lines = ARGV[1].to_i || 10000000
+max = ARGV[2].to_i || 1000
 
 File.open(filename, 'w') do |f|
-    for i in 1..10000000 do
-        f.write("#{Random.rand(1000).to_s}\n")
+    lines.times do
+        f.write("#{Random.rand(max).to_s}\n")
     end
 end
