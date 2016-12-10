61d6a880bf72dd71823f9b0f3cd32c3c4e6edf18
Fernando Crespo Grávalos <fcgravalos@email.com>
2016-12-10 12:35:23 +0100

    silly bug

--- file_generator.rb
+++ file_generator.rb
@@ -1,6 +1,6 @@
 filename = File.expand_path(ARGV[0])
-lines = ARGV[1].to_i || 10000000
-max = ARGV[2].to_i || 1000
+lines = ARGV[1].to_i | 10000000
+max = ARGV[2].to_i | 1000
 
 File.open(filename, 'w') do |f|
   lines.times do
